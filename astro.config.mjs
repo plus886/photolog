e1ea9dcb3adf461ce75f5e58c8cb095e206fa0d2
plus886 <cubicberry@gmail.com>
2025-04-25 01:05:14 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 import svelte from "@astrojs/svelte";
 import Icons from "unplugin-icons/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss(), Icons({ compiler: "svelte" })],
   },
-  integrations: [svelte()],
+  integrations: [svelte(), sitemap()],
   site: "https://photo.kokaiji.tw",
 });
