e86fee717a1ac5a710dcb25e7261001202c49376
plus886 <cubicberry@gmail.com>
2025-05-05 16:35:56 +0800

    block

--- src/components/Day/Image.svelte
+++ src/components/Day/Image.svelte
@@ -20,13 +20,14 @@
   };
 </script>
 
-<img
-  src={`${image.url}?w=1024`}
-  alt={slug}
-  class={{
-    "max-h-[50dvh] w-[90dvw] object-contain transition-all md:max-h-[70dvh] md:w-full": true,
-    "opacity-0": $isDayImageLoading,
-  }}
-  style={`view-transition-name: days_${id}_container`}
-  use:onload
-/>
+<div style={`view-transition-name: days_${id}_container`}>
+  <img
+    src={`${image.url}?w=1024`}
+    alt={slug}
+    class={{
+      "block max-h-[50dvh] w-[90dvw] object-contain transition-all md:max-h-[70dvh] md:w-full": true,
+      "opacity-0": $isDayImageLoading,
+    }}
+    use:onload
+  />
+</div>
